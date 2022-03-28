ce9eaba87664306f5f50b619214f17dedeb5b52f
DESKTOP-94T42NM\Admin <kokicola@naver.com>
2022-03-28 15:56:20 +0900

    220328 aston 가로스크롤 페이지 테스트 ㅠㅠ

--- portfolio/aston/js/index.js
+++ portfolio/aston/js/index.js
@@ -12,31 +12,67 @@ mHtml.animate({scrollTop : 0}, 1000);
 $(window).on("wheel", function(e) {
     if(mHtml.is(":animated")) return;
     if(e.originalEvent.deltaY > 0) {
-    // deltaY > 0 휠을 아래로 스크롤할때
-        if(page == 7){
-            // page = 0;
-            return
-        }
-        // return;
-        page++;
+    // deltaY > 0 휠을 아래로 스크롤할때 , 
+    // deltaY는 마우스휠을 어느방향으로 얼만큼 굴렸는지에 대한 값,
+    // 양수이면 아래쪽, 음수이면 위쪽으로 굴린 상태.
+    if(page == 3){
+        let clientWidth = document.documentElement.clientWidth;
+        let scrollWidth = document.documentElement.scrollWidth;
+        let widthResult = ((clientWidth/scrollWidth)*100)
+        let pageTheeeIdx = 0;
+        $(window).on("wheel", function(e) {
+            // if(mHtml.is(":animated")) return;
+            if(e.originalEvent.deltaY > 0) {
+                section3.style.transition = "2s ease-in-out";
+                section3.style.transform = "translate3d(-" + ((clientWidth/2)-100) + "px, 0, 0)";
+                section3.classList.add("end")
+                if(section3.classList.contains("end")){
+                    $(window).on("wheel", function(e) {
+                        if(e.originalEvent.deltaY > 0) {
+                            console.log(clientWidth)
+                            console.log(scrollWidth)
+                            page == 4;
+                            return true;
+                        }
+                    })
+                }
+            }else if(e.originalEvent.deltaY < 0) {
+                section3.style.transform = "translate3d(0px, 0, 0)";
+                section3.classList.remove("end")
+            }
+        })
+        return false;
+    }
+
+
+    if(page == 7) return;
+    page++;
+
     }else if(e.originalEvent.deltaY < 0) {
     // deltaY < 0 휠을 위로 스크롤할때
         if(page == 1) return;
+        if(page == 3) return;
         page--;
     }
-    if(page==3){
-        e.preventDefault
-        setTimeout(()=>{
-            // console.log("꺆")
-            // console.log(scrollWidth)
-            // console.log(section3)
-            // section3.style.transition = "1s ease-in-out";
-            // section3.style.transform = "translate3d(-650px, 0, 0)";
-            // progressGuage.style.height= ((posTop/scrollWidth)*100)+"%"
-        }, 500)
-        // progressGuage.style.height= ((posTop/scrollHeight)*100)+"%"
-        // return;
-    }
+
+    // function pageThreeScrollEvent(page){
+    //     if(page==3){
+    //         window.addEventListener('wheel', function(e){
+    //             e.preventDefault();
+    //             e.stopPropagation();
+    //         },{passive : false})
+    //         $(window).on("wheel", function(e) {
+    //             if(e.originalEvent.deltaY > 0) {
+    //                 section3.style.transition = "2s ease-in-out";
+    //                 section3.style.transform = "translate3d(-650px, 0, 0)";
+    //             }else if(e.originalEvent.deltaY < 0) {
+    //                 section3.style.transform = "translate3d(650px, 0, 0)";
+    //             }
+    //         })
+    //         if(mHtml.is(":animated")) return;
+    //     }
+    // }
+
     pageMove(page);
     // 페이지 높이만큼 스크롤
     var posTop =(page-1) * $(window).height();
@@ -45,12 +81,11 @@ $(window).on("wheel", function(e) {
     
     // console.log(posTop)
     let windowScrollTop = document.documentElement.scrollTop;
-    let scrollHeight = document.documentElement.scrollHeight; //총 스크롤 높이
     let clientHeight = document.documentElement.clientHeight; //현재 화면 높이
+    let scrollHeight = document.documentElement.scrollHeight; //총 스크롤 높이
+    let clientWidth = document.documentElement.clientWidth;
     let scrollWidth = document.documentElement.scrollWidth;
-    let section3 = document.querySelector('.section_3');
     // let scrolled = (windowScrollTop / height) * 100;
-    
     // progressGuage.style.height= (posTop/(clientHeight)*100)+"%"
     
     // console.log()
@@ -62,6 +97,23 @@ $(window).on("wheel", function(e) {
     // 가로 게이지 쓸때 참고
     // progressGuage.style.height= ((posTop/scrollHeight)*100)+"%"
 })
+
+let section3 = document.querySelector('.section_3');
+
+let clientHeight = document.documentElement.clientHeight;
+let scrollHeight = document.documentElement.scrollHeight;
+let clientWidth = document.documentElement.clientWidth;
+let scrollWidth = document.documentElement.scrollWidth;
+let html = document.querySelector('html');
+
+console.log(clientWidth)
+html.addEventListener('change', function(){
+    console.log("바뀜")
+})
+console.log(scrollWidth)
+console.log(clientHeight)
+console.log(scrollHeight)
+console.log("뭐지")
 
 
 // 사이드 프로그레스바
